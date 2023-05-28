54baee636544c569cbd9f96d9d49523d5eed981a
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-05-29 02:39:01 +0600

    cart api is added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-onjpk5k-shard-00-00.xskcn3u.mongodb.net:27017,ac-onjpk5k-shard-00-01.xskcn3u.mongodb.net:27017,ac-onjpk5k-shard-00-02.xskcn3u.mongodb.net:27017/?ssl=true&replicaSet=atlas-g07jbs-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,12 +31,14 @@ async function run() {
     await client.connect();
     const menuCollection = client.db('bistroDb').collection('menu')
     const reviewCollection = client.db('bistroDb').collection('reviews')
+    const cartCollection = client.db('bistroDb').collection('carts')
 
     app.get('/menu', async(req,res)=> {
 
         const result = await menuCollection.find().toArray()
         res.send(result)
     })
+   
     app.get('/reviews', async(req,res)=> {
 
         const result = await reviewCollection.find().toArray()
@@ -44,7 +46,31 @@ async function run() {
     })
 
    
-   
+  //  cart information 
+
+  app.post('/carts', async(req,res)=> {
+    const item = req.body 
+    console.log(item) 
+    const result = await cartCollection.insertOne(item)
+    res.send(result)
+  })
+
+  app.get('/carts', async(req,res)=> {
+    const email = req.query.email
+    if(!email){
+      res.send([])
+    }
+    const query = {email : email}
+    const result = await cartCollection.find(query).toArray()
+    res.send(result)
+  })
+
+  app.delete('/carts/:id', async(req,res)=>{
+    const id = req.params.id 
+    const query = {_id : new ObjectId(id)}
+    const result = await cartCollection.deleteOne(query)
+    res.send(result)
+  })
    
    
     // Send a ping to confirm a successful connection
